268f96f09ce159b0da8703ad8b6f3d2444a8bf07
NotVadusha <vadimkbondarchuk@gmail.com>
2023-11-02 16:04:02 +0200

    add methods for db

--- classes/user.py
+++ classes/user.py
@@ -2,8 +2,11 @@ import uuid
 
 
 class user:
-    def __init__(self, full_name_input, point_name_input, phone_num_input):
-        self.id = uuid.UUID
+    def __init__(self, full_name_input, point_name_input, phone_num_input, def_id):
+        self.id = def_id
         self.full_name = full_name_input
         self.point_name = point_name_input
         self.phone_num = phone_num_input
+
+    def get_fname(self):
+        return self.full_name
