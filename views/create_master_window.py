f82868fe5c06dbc0f610c86c00f8f00f6c6eb377
NotVadusha <vadimkbondarchuk@gmail.com>
2023-11-06 19:07:18 +0200

    added create class windows

--- views/create_master_window.py
+++ views/create_master_window.py
@@ -1,11 +1,57 @@
+import tkinter
+import tkinter.messagebox
+
 from classes.master import *
 from UI_primitives import *
 from pymongo import *
 
 
-def create_client_window(mongo: MongoClient):
-    create_window = create_new_window("400x300")
+def create_master_window(mongo: MongoClient):
+    created_window = create_new_window("400x500")
     masters = mongo["classes_db"]["masters"]
 
-    new_client = master()
-    masters.insert_one(new_client)
+    x = 75
+    y = 50
+    create_label(created_window, "Create new master", x, y-30, 's')
+    create_label(created_window, "Full name", x, y, 's')
+    name_inp = create_input(created_window, "Oleksiy Serhiy Danylo", x+27, y+30)
+
+    create_label(created_window, "Point name", x, y+60, 's')
+    point_inp = create_input(created_window, "Place №4", x+27, y+90)
+
+    create_label(created_window, "Address", x, y+120, 's')
+    address_inp = create_input(created_window, "Based street", x+27, y+150)
+
+    create_label(created_window, "Phone num", x, y+180, 's')
+    phone_inp = create_input(created_window, "+38", x+27, y+210)
+
+    create_label(created_window, "Specialization", x, y+240, 's')
+    specs_inp = create_input(created_window, "Color prints", x+27, y+270)
+
+    def create_object():
+        if name_inp.get() == "":
+            tkinter.messagebox.showerror("Пункт ксерокопії", "Name field is empty")
+        if point_inp.get() == "":
+            tkinter.messagebox.showerror("Пункт ксерокопії", "Point name field is empty")
+        if address_inp.get() == "":
+            tkinter.messagebox.showerror("Пункт ксерокопії", "Address field is empty")
+        if phone_inp.get() == "":
+            tkinter.messagebox.showerror("Пункт ксерокопії", "Phone field is empty")
+        if specs_inp.get() == "":
+            tkinter.messagebox.showerror("Пункт ксерокопії", "Specialization field is empty")
+
+        try:
+            name = name_inp.get()
+            point = point_inp.get()
+            address = address_inp.get()
+            phone = phone_inp.get()
+            specs = specs_inp.get()
+            masters.insert_one(master(full_name_input=name, point_name_input=point,
+                                      address_input=address, phone_num_input=phone,
+                                      specialization_input=specs).to_dict())
+            created_window.destroy()
+        except Exception as e:
+            tkinter.messagebox.showerror("Пункт ксерокопії", str(e))
+
+    create_button(created_window, create_object, 10, "Calibri", "Create", x+72, y+320)
+    create_button(created_window, created_window.destroy, 5, "Calibri", "Exit", x+94, y+370)
--- views/create_service_window.py
+++ views/create_service_window.py
@@ -1,11 +1,58 @@
+import tkinter
+import tkinter.messagebox
+from tkcalendar import Calendar
+
 from classes.service import *
 from UI_primitives import *
 from pymongo import *
 
 
 def create_client_window(mongo: MongoClient):
-    create_window = create_new_window("400x300")
-    services = mongo["classes_db"]["clients"]
+    created_window = create_new_window("400x600")
+    services = mongo["classes_db"]["services"]
 
-    new_client = service()
-    services.insert_one(new_client)
+    x = 75
+    y = 50
+    create_label(created_window, "Create new service", x, y-30, 's')
+    create_label(created_window, "Name", x, y, 's')
+    name_inp = create_input(created_window, "Derek photo copy", x+27, y+30)
+
+    create_label(created_window, "Type", x, y+60, 's')
+    type_inp = create_input(created_window, "Photo copy", x+27, y+90)
+
+    create_label(created_window, "Cost (only numbers)", x, y+120, 's')
+    cost_predefined = IntVar()
+    cost_inp = Entry(created_window, bg='#C1C1C1', width=30, textvariable=cost_predefined)
+    cost_inp.pack()
+    cost_inp.place(x=x+27, y=y+150)
+
+    create_label(created_window, "Appointed to", x, y+180, 's')
+    cal = Calendar(created_window, selectmode='day', year=2020, month=5, day=22)
+    cal.pack()
+    cal.place(x=x-10, y=y+210)
+
+    def create_object():
+        if name_inp.get() == "":
+            tkinter.messagebox.showerror("Пункт ксерокопії", "Name field is empty")
+        if type_inp.get() == "":
+            tkinter.messagebox.showerror("Пункт ксерокопії", "Type field is empty")
+        if cost_inp.get() == "":
+            tkinter.messagebox.showerror("Пункт ксерокопії", "Cost field is empty")
+        if cal.get_date() == "":
+            tkinter.messagebox.showerror("Пункт ксерокопії", "Calendar field is empty")
+
+        try:
+            name = name_inp.get()
+            serv_type = type_inp.get()
+            cost = float(cost_inp.get())
+            date = cal.get_date()
+            services.insert_one(service(service_type_input=serv_type, service_name=name,
+                                        service_cost=cost, service_appointed_to=date).to_dict())
+            created_window.destroy()
+        except TypeError:
+            tkinter.messagebox.showerror("Пункт ксерокопії", "Input cost as a number")
+        except Exception as e:
+            tkinter.messagebox.showerror("Пункт ксерокопії", str(e))
+
+    create_button(created_window, create_object, 10, "Calibri", "Create", x+72, y+410)
+    create_button(created_window, created_window.destroy, 5, "Calibri", "Exit", x+94, y+460)
